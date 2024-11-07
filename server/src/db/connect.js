15f00b41719281c7a7f9367897d1112819366d56
Web-Dev-Learner <shribabe2@gmail.com>
2024-11-07 21:22:09 +0530

    Initial commit

---
+++ server/src/db/connect.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+// Middleware
+// const db = 'mongodb+srv://grocery-webapp:grocery-webapp@cluster0.oioedos.mongodb.net/grocery-webapp?retryWrites=true&w=majority'
+
+const db = 'mongodb+srv://shrinedhimr:7iyoIaiMQVuMAOtg@cluster0.cjbnn.mongodb.net/'
+
+// Connect to MongoDB using the connection string
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log(`Connection successful`);
+}).catch((e) => {
+  console.log(`No connection: ${e}`);
+});
+
+// mongodb://localhost:27017
